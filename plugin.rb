fef78eef449581644914674c38082e1dbc9d1ffe
Your Name <you@example.com>
2026-02-21 17:33:05 +0200

    Add backend TopicQuery Location Filters

--- plugin.rb
+++ plugin.rb
@@ -43,4 +43,40 @@ after_initialize do
   add_to_serializer(:topic_view, :location_city) do
     object.topic.custom_fields["location_city"]
   end
+
+  # Allow frontend to pass location params to TopicQuery
+  add_to_class(:list_controller, :build_topic_list_options) do
+    options = super
+    options[:location_country] = params[:location_country] if params[:location_country].present?
+    options[:location_region] = params[:location_region] if params[:location_region].present?
+    options[:location_city] = params[:location_city] if params[:location_city].present?
+    options
+  end
+
+  # Filter the database queries based on locations
+  if defined?(TopicQuery)
+    TopicQuery.add_custom_filter(:location_country) do |results, topic_query|
+      if topic_query.options[:location_country].present?
+        results = results.joins("INNER JOIN topic_custom_fields tcf_country ON tcf_country.topic_id = topics.id")
+                         .where("tcf_country.name = 'location_country' AND tcf_country.value = ?", topic_query.options[:location_country])
+      end
+      results
+    end
+
+    TopicQuery.add_custom_filter(:location_region) do |results, topic_query|
+      if topic_query.options[:location_region].present?
+        results = results.joins("INNER JOIN topic_custom_fields tcf_region ON tcf_region.topic_id = topics.id")
+                         .where("tcf_region.name = 'location_region' AND tcf_region.value = ?", topic_query.options[:location_region])
+      end
+      results
+    end
+
+    TopicQuery.add_custom_filter(:location_city) do |results, topic_query|
+      if topic_query.options[:location_city].present?
+        results = results.joins("INNER JOIN topic_custom_fields tcf_city ON tcf_city.topic_id = topics.id")
+                         .where("tcf_city.name = 'location_city' AND tcf_city.value = ?", topic_query.options[:location_city])
+      end
+      results
+    end
+  end
 end
